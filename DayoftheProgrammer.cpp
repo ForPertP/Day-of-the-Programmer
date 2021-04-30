ac6e987c341dd3098468381ad82e28d2811b6827
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-04-30 16:04:54 +0900

    Create DayoftheProgrammer.cpp

---
+++ DayoftheProgrammer.cpp
@@ -0,0 +1,19 @@
+string dayOfProgrammer(int year)
+{
+    string result;
+    
+    if (year % 4 == 0 && (year < 1918 || year % 400 == 0 || year % 100 != 0))
+    {
+        result = "12.09." + std::to_string(year);
+    }
+    else if (year == 1918)
+    {
+        result = "26.09.1918";
+    }
+    else
+    {
+        result = "13.09." + std::to_string(year);
+    }
+    
+    return result;
+}
